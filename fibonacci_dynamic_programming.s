8d744cf3fd6a6d4aad7131df94e19ee5e3c3eb24
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2020-10-03 10:19:39 +0530

    Fibonacci using DP added

--- fibonacci_dynamic_programming.s
+++ fibonacci_dynamic_programming.s
@@ -1,3 +1,6 @@
+#============================
+#   Code By : Little Angel  #
+#============================
 .data
 newline:
     .asciiz "\n"
@@ -13,24 +16,48 @@ main:
     la $a0, msg
     li $v0, 4
     syscall
-
+    
     li $v0, 5
     syscall
     move $a0, $v0
+
+    slti $t0, $a0, 2
+    bne $t0, $zero, print
+    jal prep
 
 
 prep:
     move $a1, $a0
     sll $a1, $a1, 2
     addi $a1, $a1, 4
+    # move $a0, $a1
+    # j print
     sub $sp, $sp, $a1
     lw $zero, 0($sp)
+    li $t0, 1
+    sw $t0, 4($sp)
+    li $a0, 8
+    addi $sp, $sp, 8
+    j fib
 
 fib:
+    move $s0, $a0
+    lw $a0, -4($sp)
+    slt $t0, $s0, $a1
+    beq $t0, $zero, print
 
+    move $a0, $s0
+    lw $t0, -4($sp)
+    lw $t1, -8($sp)
+    add $t0, $t0, $t1
+    sw $t0, 0($sp)
+    addi $a0, $a0, 4
+    addi $sp, $sp, 4
+    j fib
 
 print:    
-    move $s0, $v0
+    move $s0, $a0
+
     la $a0, output
     li $v0, 4
     syscall
@@ -42,6 +69,8 @@ print:
     la $a0, newline
     li $v0, 4
     syscall
+
+    j Exit
 
 Exit:
     li $v0, 10
